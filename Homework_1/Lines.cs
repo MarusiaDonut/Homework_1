3badf59cf4473f6730ab811922affd1119995a1f
MYudina <MYudina@PC_MSK-20470>
2023-05-17 16:36:10 +0300

    Add project files.

---
+++ Homework_1/Lines.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1
+{
+    internal class Lines
+    {
+        public void oneLine(Int32 size, string text)
+        {
+            int borderWidth = (size - 1) * 2 + text.Length + 2;
+            int borderHeight = (size - 1) * 2 + 3;
+            for (int j = 0; j < borderHeight; j++)
+            {
+                for (int i = 0; i < borderWidth; i++)
+                {
+                    if (j == 0 || j == borderHeight - 1)
+                    {
+                        Console.Write("+");
+                    }
+                    else if (i == 0 || i == borderWidth - 1)
+                    {
+                        Console.Write("+");
+                    }
+                    else if (i == size && j == size)
+                    {
+                        Console.Write(text);
+                        i = size - 1 + text.Length;
+                    }
+                    else Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        public void twoLine(Int32 size, string text)
+        {
+            int borderWidth = (size - 1) * 2 + text.Length + 2;
+            int borderHeight = (size - 1) * 2 + 3;
+            for (int j = 0; j < borderHeight; j++)
+            {
+                for (int i = 0; i < borderWidth; i++)
+                {
+                    if (j == 0 || j == borderHeight - 1)
+                    {
+                        Console.Write("+");
+                    }
+                    else if (i == 0 || i == borderWidth - 1)
+                    {
+                        Console.Write("+");
+                    }
+                    else if ((i + j) % 2 == 0)
+                    {
+                        Console.Write("+");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void threeLine(Int32 size, string text)
+        {
+            int borderWidth = (size - 1) * 2 + text.Length + 2;
+            int n = 1;
+            for (int j = 0; j < borderWidth; j++)
+            {
+                for (int i = 0; i < borderWidth; i++)
+                {
+
+                    if (j == 0 || j == borderWidth - 1)
+                    {
+                        Console.Write("+");
+                    }
+                    else if (i == 0 || i == borderWidth - 1)
+                    {
+                        Console.Write("+");
+                    }
+                    else if (j == i)
+                    {
+                        Console.Write("+");
+                    }
+                    else if (i == borderWidth - n - 1 && j == n || i == borderWidth - n - 2 && j == n + 1)
+                    {
+                        Console.Write("+");
+                        n++;
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Homework_1/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Homework_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isNumber = false;
+            bool isEmpty = false;
+            bool isChar = false;
+            int number = 0;
+            string textInfo = "";
+            string text = "";
+            Lines lines = new Lines();
+            do
+            {
+                Console.WriteLine("Введите размерность таблицы (от 1 до 6):");
+                text = Console.ReadLine();
+                if ((Int32.TryParse(text, out number)) && (number > 0 && number < 7))
+                {
+                    //Console.WriteLine("Вы ввели правильную размерность таблицы - {0}", number);
+                    break;
+                }
+                else
+                {
+                    isNumber = false;
+                }
+            }
+            while (isNumber == false);
+
+            do
+            {
+                Console.WriteLine("Введите произвольный текст:");
+                textInfo = Console.ReadLine();
+                if (!String.IsNullOrEmpty(textInfo) && textInfo.Length + text.Length <= 38)
+                {
+                    //Console.WriteLine("Вы ввели текст: {0}", textInfo);
+                    break;
+                }
+                else
+                {
+                    isEmpty = false;
+                }
+            }
+            while (isEmpty == false);
+
+            do
+            {
+                Console.WriteLine("Введите число (от 1 до 3):");
+                string text1 = Console.ReadLine();
+                if ((Int32.TryParse(text1, out int numberS)) && (numberS > 0 && numberS < 4))
+                {
+                    switch (numberS)
+                    {
+                        case 1:
+                            lines.oneLine(number, textInfo);
+                            break;
+                        case 2:
+                            lines.twoLine(number, textInfo);
+                            break;
+                        case 3:
+                            lines.threeLine(number, textInfo);
+                            break;
+                    }
+                }
+                else
+                {
+                    isChar = false;
+                }
+
+            }
+            while (isChar == false);
+
+            Console.ReadKey();
+        }
+    }
+}
